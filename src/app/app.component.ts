ca918e3c35a4d3aa0d937142349b27d7d0cff107
DESKTOP-NHIOVE5\Salih Pehlivan <salih03455@gmail.com>
2018-04-07 00:03:22 +0300

    tarihin split edilmesi gereksiz olduğu için kaldırıldı (optimizasyon)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,9 +27,8 @@ export class AppComponent {
 			this.data = data.value;
 			let newDateArr = [];
 			for (const item of data.value) {
-				let a = item.fields.ItemStartDate.split('T')[0];
-				if (newDateArr.indexOf(a) === -1) {
-					newDateArr.push(a);
+				if (newDateArr.indexOf(item.fields.ItemStartDate) === -1) {
+					newDateArr.push(item.fields.ItemStartDate);
 				}
 			}
 			this.dates = newDateArr;
@@ -40,8 +39,7 @@ export class AppComponent {
 		let newMealArr = [];
 		let newCategoryArr = [];
 		for (const item of this.data) {
-			let shortDate = item.fields.ItemStartDate.split('T')[0];
-			if (shortDate === val) {
+			if (item.fields.ItemStartDate === val) {
 				newMealArr.push(item);
 				if (newCategoryArr.indexOf(item.fields.FoodCategory) === -1) {
 				newCategoryArr.push(item.fields.FoodCategory);
